1802281126182ee9576fec38f78a0cf9ac032a0d
Christopher Bagge <chris@bagge.com>
2021-12-23 15:46:36 -0500

    frontend buid folder

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
